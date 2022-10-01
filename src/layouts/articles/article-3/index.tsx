bf9fc00a753f261392e84bb6e375718fe9e3845b
anishs95 <anish.s95@gmail.com>
2022-10-01 13:56:51 +0530

    changed app name package name and icon

--- src/layouts/articles/article-3/index.tsx
+++ src/layouts/articles/article-3/index.tsx
@@ -1,68 +1,59 @@
-import React from 'react';
-import { ImageBackground, Platform, View } from 'react-native';
-import { Input, Layout, StyleService, Text, useStyleSheet } from '@ui-kitten/components';
-import { KeyboardAvoidingView } from './extra/keyboard-avoiding-view.component';
-import { CommentList } from './extra/comment-list.component';
-import { Article } from './extra/data';
+import React from "react";
+import { ImageBackground, Platform, View } from "react-native";
+import {
+  Input,
+  Layout,
+  StyleService,
+  Text,
+  useStyleSheet,
+} from "@ui-kitten/components";
+import { KeyboardAvoidingView } from "./extra/keyboard-avoiding-view.component";
+import { CommentList } from "./extra/comment-list.component";
+import { Article } from "./extra/data";
 
 const data: Article = Article.howToEatHealthy();
 
-const keyboardOffset = (height: number): number => Platform.select({
-  android: 0,
-  ios: height,
-});
+const keyboardOffset = (height: number): number =>
+  Platform.select({
+    android: 0,
+    ios: height,
+  });
 
 export default (): React.ReactElement => {
-
   const styles = useStyleSheet(themedStyles);
   const [inputComment, setInputComment] = React.useState<string>();
 
-  const renderCommentsLabel = React.useCallback(evaProps => (
-    <Text 
-      {...evaProps}
-      style={styles.commentInputLabel}>
-      Comments
-    </Text>
-  ), []);
+  const renderCommentsLabel = React.useCallback(
+    (evaProps) => (
+      <Text {...evaProps} style={styles.commentInputLabel}>
+        Comments
+      </Text>
+    ),
+    []
+  );
 
   const renderHeader = (): React.ReactElement => (
-    <Layout
-      style={styles.header}
-      level='1'>
-      <Text
-        style={styles.titleLabel}
-        category='h4'>
+    <Layout style={styles.header} level="1">
+      <Text style={styles.titleLabel} category="h4">
         {data.title}
       </Text>
-      <Text
-        style={styles.descriptionLabel}
-        category='s1'>
+      <Text style={styles.descriptionLabel} category="s1">
         {data.description}
       </Text>
-      <ImageBackground
-        style={styles.image}
-        source={data.image}
-      />
-      <Text style={styles.contentLabel}>
-        {data.content}
-      </Text>
+      <ImageBackground style={styles.image} source={data.image} />
+      <Text style={styles.contentLabel}>{data.content}</Text>
       <View style={styles.authoringContainer}>
-        <Text
-          appearance='hint'
-          category='p2'>
+        <Text appearance="hint" category="p2">
           {`By ${data.author.fullName}`}
         </Text>
-        <Text
-          style={styles.dateLabel}
-          appearance='hint'
-          category='p2'>
+        <Text style={styles.dateLabel} appearance="hint" category="p2">
           {data.date}
         </Text>
       </View>
       <Input
         style={styles.commentInput}
         label={renderCommentsLabel}
-        placeholder='Write your comment'
+        placeholder="Write your comment here"
         value={inputComment}
         onChangeText={setInputComment}
       />
@@ -70,9 +61,7 @@ export default (): React.ReactElement => {
   );
 
   return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      offset={keyboardOffset}>
+    <KeyboardAvoidingView style={styles.container} offset={keyboardOffset}>
       <CommentList
         style={styles.list}
         data={data.comments}
@@ -85,7 +74,7 @@ export default (): React.ReactElement => {
 const themedStyles = StyleService.create({
   container: {
     flex: 1,
-    backgroundColor: 'background-basic-color-2',
+    backgroundColor: "background-basic-color-2",
     paddingBottom: 8,
   },
   list: {
@@ -108,7 +97,7 @@ const themedStyles = StyleService.create({
     margin: 24,
   },
   authoringContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginHorizontal: 24,
   },
   dateLabel: {
@@ -117,7 +106,7 @@ const themedStyles = StyleService.create({
   commentInputLabel: {
     fontSize: 16,
     marginBottom: 8,
-    color: 'text-basic-color',
+    color: "text-basic-color",
   },
   commentInput: {
     marginHorizontal: 24,
